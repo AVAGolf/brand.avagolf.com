97343b6639c963902b06ab8ea51d90ba0fc0f359
JR Charles <29494771+jrcharles@users.noreply.github.com>
2026-05-28 18:58:26 -0400

    change title

--- src/lib/sections.ts
+++ src/lib/sections.ts
@@ -7,7 +7,7 @@ export interface Section {
 }
 
 export const sections: Section[] = [
-  { slug: "", title: "Introduction", group: "Core Identity" },
+  { slug: "", title: "Brand Guidelines", group: "Core Identity" },
   { slug: "logo-marks", title: "Logo Marks", group: "Core Identity" },
   { slug: "colors", title: "Colors", group: "Core Identity" },
   { slug: "typography", title: "Typography", group: "Core Identity" },
