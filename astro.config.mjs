5fccabf28cddc7aefbc0544c8758339b93b84872
Aron Schüler <git@lakur.tech>
2026-05-28 17:49:06 +0200

    feat: add SEO things and favicions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://brand.avagolf.com',
   trailingSlash: 'always',
   build: {
     format: 'directory',
   },
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
